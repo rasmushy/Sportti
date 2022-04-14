87ea1e8e19b951673f171d2d9fdda5c3e2725cc5
Jukka-Pekka Jaakkola <jukka-pekka.jaakkola@outlook.com>
2022-04-14 11:19:41 +0300

    Added empty history activity.

---
+++ app/src/main/java/fi/sportti/app/ui/activities/HistoryActivity.java
@@ -0,0 +1,15 @@
+package fi.sportti.app.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import fi.sportti.app.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
