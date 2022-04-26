29f64dd2816cfcc42c39e2acf43e59227b86f887
Yana Krylova <krylovina@gmail.com>
2022-04-26 09:48:38 +0300

    User Profile added, work in progress

--- app/src/main/java/fi/sportti/app/ui/activities/MainActivity.java
+++ app/src/main/java/fi/sportti/app/ui/activities/MainActivity.java
@@ -5,56 +5,48 @@
 
 package fi.sportti.app.ui.activities;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
 
-
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import fi.sportti.app.R;
 import fi.sportti.app.datastorage.room.User;
-import fi.sportti.app.location.LocationTracking;
 import fi.sportti.app.ui.viewmodels.MainViewModel;
 
-/**
- * @author lassib
+/*
+ * @author rasmushy, lassib
  */
-
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class MainActivity extends AppCompatActivity {
-    private static final int PERMISSION_READ_PHONE_STATE = 101;
+
     private static final String TAG = "MainActivity"; // TAG for Log.d
+
     private static MainViewModel mainViewModel;
+
     private User user;
-    private AlertDialog dialog;
+    private List<User> userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userList = new ArrayList<>();
         //Setup our access to database
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        mainViewModel.getAllUsers().observe(this, users -> userList = users);
         Log.d(TAG, "onCreate() launched");
         initialStartUp();
-        checkAppPermissions();
     }
 
     /*
@@ -65,15 +57,16 @@ public class MainActivity extends AppCompatActivity {
     private void initialStartUp() {
         Log.d(TAG, "initialStartUp() launched");
         if (mainViewModel.getFirstUser() != null) {
-            Log.d(TAG, "initialStartUp() if statement not null " + mainViewModel.getFirstUser().getUserName());
+            Log.d(TAG, "initialStartUp() if statement not null");
+            Log.d(TAG, "initialStartup() userList value: " + userList.toString());
             //Lets make another user and compare it to our first user in db
             User plainUser = new User();
             //Get our db's first user
             user = mainViewModel.getFirstUser();
             //If they are not equal then we have user that added personal info's
-            if (!Objects.equals(user, plainUser)) {
-                Log.d(TAG, "InitialStartUp() welcomes user, hi! " + user.getuid());
-                //TODO: Welcome our user in a textview or something
+            if (!user.equals(plainUser)) {
+                Log.d(TAG, "InitialStartUp() welcomes user, hi!");
+                //TODO: Welcome our user
             }
             return;
         }
@@ -86,89 +79,41 @@ public class MainActivity extends AppCompatActivity {
         return mainViewModel;
     }
 
+
     /*
      *@author Lassi Bågman
      * Methods for buttons
      */
+    public void openStartExerciseActivity(View view){
+        Intent intent = new Intent(this, NewRecordedExerciseActivity.class);
+        startActivity(intent);
+    }
 
-    public void openHistoryActivity(View view) {
+    public void openSaveExerciseActivity(View view){
+        Intent intent = new Intent(this, NewManualExerciseActivity.class);
+        startActivity(intent);
+    }
+
+    public void openHistoryActivity(View view){
         Intent intent = new Intent(this, HistoryActivity.class);
         startActivity(intent);
     }
 
-    public void openProfileActivity(View view) {
-        //method here
+    public void openProfileActivity(View view){
+       Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 
     /*
      *@author Lassi Bågman
      * Method for pop up
      */
-    public void onClickNewExercise(View view) {
+    public void selectExerciseTypePopUp(View view){
         //Variables for exercise pop up
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         final View selectExercisePopUpView = getLayoutInflater().inflate(R.layout.pop_up_select_new_exercise_type, null);
         dialogBuilder.setView(selectExercisePopUpView);
-
-        Button start = (Button) selectExercisePopUpView.findViewById(R.id.buttonStartRecordingPopUp);
-        Button save = (Button) selectExercisePopUpView.findViewById(R.id.buttonSaveExercisePopUp);
-
         AlertDialog dialog = dialogBuilder.create();
         dialog.show();
-
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, NewRecordedExerciseActivity.class);
-                dialog.dismiss();
-                startActivity(intent);
-            }
-        });
-
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, NewManualExerciseActivity.class);
-                dialog.dismiss();
-                startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (dialog != null) {
-            dialog.dismiss();
-        }
-    }
-
-    /*
-     *@author Jukka-Pekka Jaakkola
-     */
-
-    private void checkAppPermissions(){
-        //At App startup check if app has READ_PHONE_STATE permission which is required to display maps.
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[] { Manifest.permission.READ_PHONE_STATE },PERMISSION_READ_PHONE_STATE);
-        }
-    }
-    @Override
-    //This method is called by Android when user responds to permission request.
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode == PERMISSION_READ_PHONE_STATE){
-            if(!permissionGranted(grantResults)){
-                String message = getResources().getString(R.string.toast_maps_not_available);
-                Toast toast = Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        }
-    }
-
-    private boolean permissionGranted(int[] grantResults){
-        return grantResults[0] == PackageManager.PERMISSION_GRANTED;
     }
 }
