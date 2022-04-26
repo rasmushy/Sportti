42553ac79860b6d1556ade8878126ed3e20002cb
Rasmus Hyyppä <rasmus.hyyppa@metropolia.fi>
2022-04-26 13:27:48 +0300

    Minor update on MainActivity, removing useless ArrayLists.

--- app/src/main/java/fi/sportti/app/ui/activities/MainActivity.java
+++ app/src/main/java/fi/sportti/app/ui/activities/MainActivity.java
@@ -21,14 +21,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import fi.sportti.app.R;
 import fi.sportti.app.datastorage.room.User;
 import fi.sportti.app.ui.viewmodels.MainViewModel;
 
-/*
+/**
  * @author rasmushy, lassib
  */
 @RequiresApi(api = Build.VERSION_CODES.O)
@@ -36,27 +33,22 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_READ_PHONE_STATE = 101;
     private static final String TAG = "MainActivity"; // TAG for Log.d
 
-    private static MainViewModel mainViewModel;
+    private static MainViewModel mainViewModel; // Main view model for database
 
     private User user;
     private AlertDialog dialog;
-    private List<User> userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        userList = new ArrayList<>();
-        //Setup our access to database
+        //Initialize our access to database
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-
-        mainViewModel.getAllUsers().observe(this, users -> userList = users);
-        Log.d(TAG, "onCreate() launched");
         initialStartUp();
         checkAppPermissions();
     }
 
-    /*
+    /**
      * @author Rasmus Hyyppä
      * Initial startup method to check do we have user that has saved personal information
      * If not, we create blank user, that can visit Application's activities freely.
@@ -65,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "initialStartUp() launched");
         if (mainViewModel.getFirstUser() != null) {
             Log.d(TAG, "initialStartUp() if statement not null");
-            Log.d(TAG, "initialStartup() userList value: " + userList.toString());
             //Lets make another user and compare it to our first user in db
             User plainUser = new User();
             //Get our db's first user
@@ -87,37 +78,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /*
-     *@author Lassi Bågman
+    /**
+     * @author Lassi Bågman
      * Methods for buttons
      */
-    public void openStartExerciseActivity(View view){
+    public void openStartExerciseActivity(View view) {
         Intent intent = new Intent(this, NewRecordedExerciseActivity.class);
         startActivity(intent);
     }
 
-    public void openSaveExerciseActivity(View view){
+    public void openSaveExerciseActivity(View view) {
         Intent intent = new Intent(this, NewManualExerciseActivity.class);
         startActivity(intent);
     }
 
-    public void openHistoryActivity(View view){
+    public void openHistoryActivity(View view) {
         Intent intent = new Intent(this, HistoryActivity.class);
         startActivity(intent);
     }
 
 
-    public void openProfileActivity(View view){
-       Intent intent = new Intent(this, ProfileActivity.class);
+    public void openProfileActivity(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
 
     }
 
-    /*
-     *@author Lassi Bågman
+    /**
+     * @author Lassi Bågman
      * Method for pop up
      */
-    public void selectExerciseTypePopUp(View view){
+    public void selectExerciseTypePopUp(View view) {
         //Variables for exercise pop up
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         final View selectExercisePopUpView = getLayoutInflater().inflate(R.layout.pop_up_select_new_exercise_type, null);
@@ -135,24 +126,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /*
-     *@author Jukka-Pekka Jaakkola
+    /**
+     * @author Jukka-Pekka Jaakkola
      */
 
-    private void checkAppPermissions(){
+    private void checkAppPermissions() {
         //At App startup check if app has READ_PHONE_STATE permission which is required to display maps.
         if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[] { Manifest.permission.READ_PHONE_STATE },PERMISSION_READ_PHONE_STATE);
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_READ_PHONE_STATE);
         }
     }
+
     @Override
     //This method is called by Android when user responds to permission request.
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode == PERMISSION_READ_PHONE_STATE){
-            if(!permissionGranted(grantResults)){
+        if (requestCode == PERMISSION_READ_PHONE_STATE) {
+            if (!permissionGranted(grantResults)) {
                 String message = getResources().getString(R.string.toast_maps_not_available);
                 Toast toast = Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT);
                 toast.show();
@@ -160,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private boolean permissionGranted(int[] grantResults){
+    private boolean permissionGranted(int[] grantResults) {
         return grantResults[0] == PackageManager.PERMISSION_GRANTED;
     }
 }
