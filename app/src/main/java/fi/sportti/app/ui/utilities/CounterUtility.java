7e22e98ccb0cdb9bbe2781f967e709c33c42cf67
lassib <91030337+lassib@users.noreply.github.com>
2022-04-28 15:34:47 +0300

    Made basic counter for inputs

--- app/src/main/java/fi/sportti/app/ui/utilities/CounterUtility.java
+++ app/src/main/java/fi/sportti/app/ui/utilities/CounterUtility.java
@@ -2,29 +2,63 @@ package fi.sportti.app.ui.utilities;
 
 public class CounterUtility {
 
-    private int value;
+    private int counter;
+    private int min, max, step, start;
+
+    public CounterUtility(int min, int max, int start, int step){
+        this.counter = start;
+        this.min = min;
+        this.max = max;
+        this.step = step;
+        this.start = start;
+    }
 
     public CounterUtility(){
-        this.value = 0;
+        this.counter = 0;
+        this.min = -100;
+        this.max = 100;
+        this.step = 1;
     }
 
-    public CounterUtility(int startValue){
-        this.value = startValue;
+    public String returnCounter(){
+        return Integer.toString(counter);
     }
 
-    public void plus(){
-        this.value++;
+    public int returnCounterInt(){ return counter;}
+
+    public void addToCounter(){
+        if(this.counter + this.step <= this.max){
+            this.counter += this.step;
+        }else{
+            this.counter = this.max;
+        }
     }
 
-    public void plus(int value){
-        this.value += value;
+    public void addToCounter(int i){
+        if(this.counter + i <= this.max){
+            this.counter += i;
+        }else{
+            this.counter = this.max;
+        }
     }
 
-    public void minus(){
-        this.value--;
+    public void minusToCounter(){
+        if(this.counter - this.step >= this.min){
+            this.counter -= this.step;
+        }else{
+            this.counter = this.min;
+        }
     }
 
-    public void minus(int value){
-        this.value -= value;
+    public void minusToCounter(int i){
+        if(this.counter - i >= this.min){
+            this.counter -= i;
+        }else{
+            this.counter = this.min;
+        }
+    }
+
+    public void resetCounter(){
+        this.counter = start;
     }
 }
